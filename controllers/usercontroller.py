c8b4bba4654427ca8c3b44c445bc7128bd118620
Jim Persson <jim@wendy.int.nurd.se>
2009-09-19 18:06:20 +0200

    * Tracking topic/bot nick/user nicks/channel users * Automatically reclaim lost nick

---
+++ controllers/usercontroller.py
@@ -0,0 +1,23 @@
+from models import IRCUser
+
+class UserController:
+	def __init__(self):
+		self.users = []
+
+	def add_user(self, user):
+		self.users.append(user)
+
+	def del_user(self, user):
+		self.users.remove(user)
+
+	def get_user(self, whostring):
+		matching = IRCUser(whostring)
+
+		try:
+			index = self.users.index(matching)
+			print "Returning old user %s" % self.users[index]
+			return self.users[index]
+		except:
+			self.add_user(matching)
+			print "Adding new user %s" % matching
+			return matching
